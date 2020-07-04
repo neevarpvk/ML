eddef00c4d19f234df03df035d35a546aa2c83c7
neevarpvk <63980879+neevarpvk@users.noreply.github.com>
2020-07-03 22:23:51 -0700

    Update Decisiontrees.py

--- Decisiontrees.py
+++ Decisiontrees.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Fri Jul  3 19:57:37 2020
+
+@author: praveenvudumu
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Sun Jun  7 17:09:14 2020
 
-@author: praveen
+@author: praveenvudumu
 """
 
 # import modules
@@ -15,11 +23,10 @@ from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import confusion_matrix, plot_confusion_matrix
 
-
 # load data and label columns
 
 # https://archive.ics.uci.edu/ml/datasets/Heart+Disease
-df = pd.read_csv("/Users/praveen/desktop/datascience/datasets/heatdisease_uciml.data", header = None)
+df = pd.read_csv("/Users/praveenvudumu/desktop/datascience/datasets/heatdisease_uciml.data", header = None)
 df.columns = [
     'age',
     'sex',
@@ -63,7 +70,7 @@ y.unique() # 0 - no hd; 1-4 different levels of hd
 y_index = y > 0 # convert to 0 if hd > 0
 y[y_index] = 1 # 1 - has hd
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42) #split 80/20
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 42) # default split
 
 print("Training features/target:", X_train.shape, y_train.shape)
 print("Testing features/target:", X_test.shape, y_test.shape)
@@ -71,7 +78,7 @@ print("Testing features/target:", X_test.shape, y_test.shape)
 
 # build classification tree
 
-df_dtc = DecisionTreeClassifier(random_state = 11)
+df_dtc = DecisionTreeClassifier(random_state = 42)
 df_dtc = df_dtc.fit(X_train, y_train)
 plot_tree(df_dtc,
          filled = True,
@@ -80,32 +87,27 @@ plot_tree(df_dtc,
          feature_names = X.columns
          ) # plot initial tree
 
-plot_confusion_matrix(df_dtc, X_test, y_test, display_labels = ["No HD", "Yes HD"]) # confusion matrix
+plot_confusion_matrix(df_dtc, X_test, y_test, cmap=plt.cm.Blues, display_labels = ["No HD", "Yes HD"]) # confusion matrix
 
 
 # Post pruning decision trees with cost complexity pruning
 ## https://scikit-learn.org/stable/auto_examples/tree/plot_cost_complexity_pruning.html
 
+#path = df_dtc.cost_complexity_pruning_path(X_train, y_train)
+#ccp_alphas, impurities = path.ccp_alphas, path.impurities
+
 path = df_dtc.cost_complexity_pruning_path(X_train, y_train)
-ccp_alphas, impurities = path.ccp_alphas, path.impurities
+ccp_alphas = path.ccp_alphas
+ccp_alphas = ccp_alphas[:-1] #remove max value of alpha
 
-fig, ax = plt.subplots()
-ax.plot(ccp_alphas[:-1], impurities[:-1], marker='o', drawstyle="steps-post")
-ax.set_xlabel("Effective alpha")
-ax.set_ylabel("Total impurity of leaves")
-ax.set_title("Total impurity vs effective alpha for training set")
-
-df_dtcs = []
-
+clfs = []
 for ccp_alpha in ccp_alphas:
-    df_dtc = DecisionTreeClassifier(random_state=0, ccp_alpha=ccp_alpha)
-    df_dtc.fit(X_train, y_train)
-    df_dtcs.append(df_dtc)
+    clf = DecisionTreeClassifier(random_state=42, ccp_alpha=ccp_alpha)
+    clf.fit(X_train, y_train)
+    clfs.append(clf)
 
-print("Number of nodes in the last tree is: {} with ccp_alpha: {}".format(df_dtcs[-1].tree_.node_count, ccp_alphas[-1]))
-
-train_scores = [df_dtc.score(X_train, y_train) for df_dtc in df_dtcs]
-test_scores = [df_dtc.score(X_test, y_test) for df_dtc in df_dtcs]
+train_scores = [clf.score(X_train, y_train) for clf in clfs]
+test_scores = [clf.score(X_test, y_test) for clf in clfs]
 
 fig, ax = plt.subplots()
 ax.set_xlabel("alpha")
@@ -119,30 +121,30 @@ plt.show()
 
 # Cross validation to find the best value of 'alpha'
 
-df_dtc = DecisionTreeClassifier(random_state = 42, ccp_alpha = 0.02)
-scores = cross_val_score(df_dtc, X_train, y_train, cv = 4) # cv = 4 because the df is pretty small; cv > 4 and cv < 4 gave lower accuracy scores
-df = pd.DataFrame(data = {'tree': range(4), 'accuracy': scores})
+df_dtc = DecisionTreeClassifier(random_state = 42, ccp_alpha = 0.016)
+scores = cross_val_score(df_dtc, X_train, y_train, cv = 5)
+df = pd.DataFrame(data = {'tree': range(5), 'accuracy': scores})
 df.plot(x = 'tree', y = 'accuracy', marker = 'o', linestyle = '--')
 
 alpha_loop = []
 for ccp_alpha in ccp_alphas:
-    dt_dtc = DecisionTreeClassifier(random_state = 42, ccp_alpha = 0.02)
+    dt_dtc = DecisionTreeClassifier(random_state = 42, ccp_alpha = ccp_alpha)
     scores = cross_val_score(df_dtc, X_train, y_train, cv = 5)
     alpha_loop.append([ccp_alpha, np.mean(scores), np.std(scores)])
 
 df_alpha = pd.DataFrame(alpha_loop, columns = ['alpha', 'mean_accuracy', 'std'])
+df_alpha.plot(x = 'alpha', y = 'mean_accuracy',
+              yerr = 'std', marker = 'o', linestyle = '--')
 
-df_alpha.plot(x = 'alpha', y = 'mean_accuracy', yerr = 'std', marker = 'o', linestyle = '--') # from this grap alpha = 0.014
+df_alpha[(df_alpha['alpha'] > 0.016) & (df_alpha['alpha'] < 0.015)] # no change in accuracy
 
-df_alpha[(df_alpha['alpha'] > 0.014) & df_alpha['alpha'] < 0.015] # no change in accuracy
-
-ccp_alpha = 0.014 #ideal alpha value
+ccp_alpha = 0.016 #ideal alpha value
 
 df_pruned = DecisionTreeClassifier(random_state = 42, ccp_alpha = ccp_alpha)
 df_pruned = df_pruned.fit(X_train, y_train)
 
 plot_confusion_matrix(df_pruned, X_test, y_test,
-                      display_labels = ["No HD", "Yes HD"])
+                      cmap=plt.cm.Blues, display_labels = ["No HD", "Yes HD"])
 
 # final pruned tree
 plot_tree(df_pruned,
@@ -150,3 +152,21 @@ plot_tree(df_pruned,
           rounded = True,
           class_names = ["No HD", "Yes HD"],
           feature_names = X.columns)
+
+
+#######################################################################################
+
+# Using RandomizedSearchCV()
+
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
+param_dist = {"max_depth": [3, None],
+              "max_features": randint(1, 9),
+              "min_samples_leaf": randint(1, 9),
+              "criterion": ["gini", "entropy"]}
+tree = DecisionTreeClassifier()
+tree_cv = RandomizedSearchCV(tree, param_dist, cv = 10)
+tree_cv.fit(X_train, y_train)
+
+print("Tuned Decision Tree Parameters: {}".format(tree_cv.best_params_))
+print("Best score is {}".format(tree_cv.best_score_))
